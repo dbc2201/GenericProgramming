5537f712056bcd8fed15bdd852e312577b732615
divyansh <divyanshb22@gmail.com>
2020-10-29 15:21:41 +0530

    sectionI: add code and data for rule 1

--- README.md
+++ README.md
@@ -14,3 +14,18 @@ A class, that operates on _parameterized types_ is called a `Generic Class`.
 ## Points about Generic Programming
 
 1. Generics only work with a reference type.
+When we want to declare an object of a "generic" class,
+the `type argument` that is passed to the `type parameter` should always be a
+_reference type_.
+We cannot use a primitive type such as `char`, `int`, `float`, or `boolean`.
+If we want to pass a primitive type value, we can use the wrapper classes available
+for the primitive type, such as
+byte -> `java.lang.Byte`
+short -> `java.lang.Short`
+int -> `java.lang.Integer`
+long -> `java.lang.Long`
+float -> `java.lang.Float`
+double -> `java.lang.Double`
+char -> `java.lang.Character`
+boolean -> `java.lang.Boolean`
+
--- src/examples/rules/Rule1.java
+++ src/examples/rules/Rule1.java
@@ -1,9 +1,27 @@
 package examples.rules;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 public class Rule1 {
     public static void main(String[] args) {
-
+        GenericClass1<Byte> byteObject = new GenericClass1<Byte>((byte) 1);
+        GenericClass1<Short> shortObject = new GenericClass1<Short>((short) 10);
+        GenericClass1<Integer> integerObject = new GenericClass1<Integer>(45);
+        GenericClass1<Long> longObject = new GenericClass1<Long>(451L);
+        GenericClass1<Float> floatObject = new GenericClass1<Float>(3.14f);
+        GenericClass1<Double> doubleObject = new GenericClass1<Double>(4.2);
+        GenericClass1<Character> characterObject = new GenericClass1<Character>('A');
+        GenericClass1<Boolean> booleanObject = new GenericClass1<Boolean>(true);
+        GenericClass1<String> stringObject = new GenericClass1<String>("Hello");
+        GenericClass1<BigInteger> bigIntegerObject = new GenericClass1<BigInteger>(BigInteger.ONE);
+        GenericClass1<BigDecimal> bigDecimalObject = new GenericClass1<BigDecimal>(BigDecimal.ONE);
+        GenericClass1<Student> studentObject = new GenericClass1<Student>(new Student());
+//        GenericClass1<int> intObj = new GenericClass1<int>(45);
     }
+}
+
+class Student {
 }
 
 class GenericClass1<T> {
